600e889fc05f91be958d46d2114fd83f4100c900
unknown <matnaranjo2020@gmail.com>
2022-03-11 15:39:52 -0500

    Unified snake behaviour

--- Assets/Scripts/Snake/SnakeEats.cs
+++ Assets/Scripts/Snake/SnakeEats.cs
@@ -6,26 +6,57 @@ using UnityEngine.UI;
 public class SnakeEats : MonoBehaviour
 {
 
+
+// Variables for snake eating, changing score text and creating new food
     [SerializeField]
     private GameObject FoodToSpawn = null;
-    [SerializeField]
-    private Text ScoreText;
-    private GameObject FoodParticle = null;
-    private GameObject[] SnakeSegments = new GameObject[] {};
 
-    private List <int> AvailableSpotsX = new List<int>();
-    private List <int> AvailableSpotsY = new List<int>();
-    private Vector3 RandomPos;
-    private int RandomSelection=0;
-    private float CoordinatesPosX=0;
-    private float CoordinatesPosY=0;
+    private GameObject FoodParticle = null; //receives the object 'food' in the screen
+    private GameObject[] SnakeSegments = new GameObject[] {}; // contains all the objects that form the snake (head and body segments)
 
+    private List <int> AvailableSpotsX = new List<int>(); // contains the available x positions in the grid to place food
+    private List <int> AvailableSpotsY = new List<int>(); // contains the available y positions in the grid to place food
+    
+    private int RandomSelection=0; // selects a random index based on the size of AvailableSpot X and Y, to place the new food
+    private float CoordinatesPosX=0; // transformed  value of int grid to transform.position.x value
+    private float CoordinatesPosY=0;// transformed  value of int grid to transform.position.y value
+    private Vector3 RandomPos; // Stores the x and y coordinate to place the new food particle
+    // Position of the food in the map, once it is placed again
     private float FoodPosX;
     private float FoodPosY;
+    //Position of the snake's head used to be compared with the foodPos
     private float HeadPosX;
     private float HeadPosY;
 
+    // Score Text and score value.
+    [SerializeField]
+    private Text ScoreText;
     private int Score=0;
+
+
+
+    // Variables for body instatement
+    [SerializeField]
+    private GameObject BodyBlock=null; // Body block to instate 
+
+    private List<GameObject> SnakeBody = new List<GameObject>(); //List of body blocks
+
+
+    // Variables for Snake movement
+    private int Move = 0; 
+    private int Refresh = 7;
+
+    private float MovementX=0f;
+    private float MovementY=0f;
+    private float PosChange = 0.4f;
+
+    private float CurrentPosX=10f;
+
+    private float CurrentPosY=10f;
+    private Vector3 PastPos;
+    private Vector3 CurrentPos;
+    private bool Alive = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -34,24 +65,38 @@ public class SnakeEats : MonoBehaviour
     }
 
     void Update(){
-        FindFood();
-        SnakeFindsFood();
+        InputGetter();  
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (!(Refresh == Move)){
+            Move+=1;
+            
+        }
+        else {
+            SnakeFindsFood();
+            Move=0;
+        }
 
+        Debug.Log(Alive); 
     }
 
     // Compares the position of the snake's head with the food's position and if they are in the same grid position, firts, the food particle is Destroyed, then, the grid matrix is updated, a point is added to the score, and a new position for a new food particle is decided.
     private void SnakeFindsFood(){
 
-        HeadPosX = transform.position.x;
-        HeadPosY = transform.position.y;
+        SnakeBodyMovement();
+        FindFood();
+
         if ((HeadPosX>FoodPosX-0.1f && HeadPosX<FoodPosX+0.1f ) && (HeadPosY>FoodPosY-0.1f && HeadPosY<FoodPosY+0.1f)){
             Destroy(FoodParticle);
             GridUpdate();
+
+            // New boddy block added to Snake Body List
+            int BodyLength=SnakeBody.Count;
+            GameObject NewBlock = Instantiate(BodyBlock,transform.position,transform.rotation);
+            SnakeBody.Add(NewBlock);
 
             Score++;
             ScoreText.text = "Score: "+ Score;
@@ -83,10 +128,12 @@ public class SnakeEats : MonoBehaviour
             FoodParticle = GameObject.FindWithTag("Food");
             FoodPosX = FoodParticle.transform.position.x;
             FoodPosY = FoodParticle.transform.position.y;
+
+            Debug.Log("X: "+ FoodPosX);
+            Debug.Log("Y: "+ FoodPosY);
+
         }
     }
-
-
 
     // Every Time the snake eats, updates the game grid, leaving empty spaces with an 'e' and the spaces where the snake is WaitWhile an'f'
     private void GridUpdate(){
@@ -106,7 +153,6 @@ public class SnakeEats : MonoBehaviour
             GridStatus.GridArray[GridPosY, GridPosX] = 'f'; 
         }
     }
-
 
 
     // Transforms the position of every snake segment into grid positions
@@ -142,5 +188,52 @@ public class SnakeEats : MonoBehaviour
                                                 {'e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e'},
                                                 {'e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e'},
                                                 {'e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e','e'}};
+    }
+
+
+
+    //Movement
+
+    private void InputGetter(){
+        if ( MovementX==0f && (Input.GetKey("left") || Input.GetKey("right")) && (CurrentPosY!=transform.position.y)){
+            MovementX = Input.GetAxisRaw("Horizontal");
+            MovementY=0f;
+            CurrentPosY = transform.position.y;
+
+
+        }
+        if (MovementY==0f && (Input.GetKey("down") || Input.GetKey("up")) && (CurrentPosX!=transform.position.x)){
+            MovementY = Input.GetAxisRaw("Vertical");
+            MovementX=0f;
+            CurrentPosX = transform.position.x;
+        }
+    }
+
+    private void SnakeBodyMovement(){
+        if (Alive){
+            PastPos = transform.position;
+            transform.position+=new Vector3(MovementX, MovementY, 0f) * PosChange;
+            for (int Segment = 0 ; Segment<SnakeBody.Count; Segment++) {
+                CurrentPos = SnakeBody[Segment].transform.position;
+                SnakeBody[Segment].transform.position = PastPos;
+                PastPos = CurrentPos;
+            }
+        }
+        CollisionsWithLimitsAndBody();
+    }
+
+    private void CollisionsWithLimitsAndBody(){
+        HeadPosX = transform.position.x;
+        HeadPosY = transform.position.y;
+        if (HeadPosX>4 || HeadPosX<-4 || HeadPosY>2.8 || HeadPosY<-5.2){
+            Alive=false;
+        }
+        foreach(GameObject Segment in SnakeBody){
+            float SegmentX = Segment.transform.position.x;
+            float SegmentY = Segment.transform.position.y;
+            if ((HeadPosX>SegmentX-0.1 && HeadPosX<SegmentX+0.1)&& (HeadPosY>SegmentY-0.1 && HeadPosY<SegmentY+0.1)){
+                Alive=false;
+            }
+        }
     }
 }
